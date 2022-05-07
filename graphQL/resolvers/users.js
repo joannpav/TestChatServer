3bddc3cf06a6d0d78fdfab098d81cee4490feec4
joannpav <joannpav@gmail.com>
2022-05-07 07:02:25 -0700

    Update users.js

--- graphQL/resolvers/users.js
+++ graphQL/resolvers/users.js
@@ -4,7 +4,8 @@ const { UserInputError } = require('apollo-server');
 const { validateLoginInput, validateRegisterInput } = require('../../util/validators');
 
 
-const { SECRET_KEY } = require('../../config');
+const { SECRET_KEY } = process.env.SECRET_KEY || require('../../config');
+
 const User = require('../../models/User');
 const Organization = require('../../models/Organization');
 
